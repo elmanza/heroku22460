8b54e68bf40b89717dd6eb82e765df308bdf72f5
Andres Manzano <amanzano@lean-tech.io>
2022-02-23 19:27:46 -0500

    v0.0.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 let cors = require("cors");
 const app = express();
-
+const PORT = 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -10,7 +10,9 @@ app.use(express.static(__dirname + "/public"));
 
 app.use(cors("*"));
 
-
+// app.get("/", (req, res, next)=>{
+//     res.sendFile
+// });
 
 app.listen(PORT, (err) => {
     if (err) return console.log('error en listen server', err);
