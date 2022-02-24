52b8968e5bb7b46d79e72ce3e9bade8a3699acb7
Andres Manzano <amanzano@lean-tech.io>
2022-02-23 19:59:06 -0500

    ..

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 let cors = require("cors");
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
